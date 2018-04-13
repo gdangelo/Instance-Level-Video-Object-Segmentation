3114066808e0ead9de8ee8e9cc6c1899c9141559
gdangelo <gregory@gdangelo.fr>
2018-04-13 23:32:39 +0200

    feat: Add settings flags for building and training network

--- src/main.py
+++ src/main.py
@@ -1,35 +1,24 @@
-import time
+import os
+import glob
+import random
+import scipy.misc
 import numpy as np
 import tensorflow as tf
-from scipy.misc import imread
-import matplotlib.pyplot as plt
 from fcn import FCN
 
-# Read images
-img1 = imread("./examples/catamaran.jpg").astype(np.float32)
-img1 = img1 - np.mean(img1)
-img2 = imread("./examples/electric_guitar.jpg").astype(np.float32)
-img2 = img2 - np.mean(img2)
+FLAGS = tf.flags.FLAGS
+tf.flags.DEFINE_string("data_dir", "./data/", "Path to dataset")
+tf.flags.DEFINE_integer("image_height", "2710", "Height of images in dataset")
+tf.flags.DEFINE_integer("image_width", "3384", "Width of images in dataset")
+tf.flags.DEFINE_string("vgg16_weights", "./weights/vgg16.npy", "Path to VGG16 weights")
+tf.flags.DEFINE_integer("num_classes", "7", "Number of classes to predict")
+tf.flags.DEFINE_integer("epochs", "50", "Number of epochs for training")
+tf.flags.DEFINE_integer("batch_size", "20", "Batch size for training")
+tf.flags.DEFINE_float("learning_rate", "1e-4", "Learning rate for SGD Optimizer")
+tf.flags.DEFINE_float("momentum", "0.9", "Momentum for SGD Optimizer")
 
-# Set dataset
-x = tf.placeholder(tf.float32, shape=(None, 1080, 1920, 3))
-resized = tf.image.resize_images(x, (224, 224))
+def main(_):
+    return None
 
-# Load FCN models with pretrained weights for VGG16
-fcn_vgg_16 = FCN().fcn_vgg_16('./weights/vgg16.npy', resized)
-
-# Test models
-with tf.Session() as sess:
-    t1 = time.time()
-    init = tf.global_variables_initializer()
-    sess.run(init)
-
-    # Fire!
-    output = sess.run(fcn_vgg_16, feed_dict={ x: [img1, img2] })
-    t2 = time.time() - t1
-    print('Model run in {0:.2f}s'.format(t2))
-
-    # Visualize
-    for i in range(output.shape[0]):
-        mat = plt.matshow(output[i])
-        plt.show()
+if __name__ == '__main__':
+    tf.app.run()
