854a0732336e929e59f015c86611e3109677c4d8
gdangelo <gregory@gdangelo.fr>
2018-04-18 22:09:28 +0200

    feat: Build FCN32, FCN16, and FCN8 based on VGG16

--- src/fcn.py
+++ src/fcn.py
@@ -27,20 +27,21 @@ class FCN:
 
     def back_conv_layer(self, input, factor, num_classes, name=None):
         # Get input shape
-        input_shape = input.get_shape().as_list()
-        batch_size = tf.shape(input)[0]
+        input_shape = tf.shape(input)
+        in_features = input.get_shape()[3].value
 
         # Get output shape
-        new_height = input_shape[1] * factor
-        new_width = input_shape[2] * factor
-        output_shape = [batch_size, new_height, new_width, num_classes]
+        new_height = ((input_shape[1] - 1) * factor) + 1
+        new_width = ((input_shape[2] - 1) * factor) + 1
+        new_shape = [input_shape[0], new_height, new_width, num_classes]
+        output_shape = tf.stack(new_shape)
 
         # Get kernel size
         ksize = 2 * factor - factor % 2
 
         # Defines weights
         weights = tf.Variable(tf.truncated_normal(
-            shape=[ksize, ksize, num_classes, input_shape[3]],
+            shape=[ksize, ksize, num_classes, in_features],
             mean=0,
             stddev=0.1))
 
@@ -55,18 +56,66 @@ class FCN:
 
         return back_conv
 
-    def fcn_vgg_16(self, features, weights, num_classes):
-        # Build vgg16
+    def load_vgg_16(self, features, weights):
+        # Build VGG with VGG16 pretrained weights
         vgg = VGG(vgg16_weights_file=weights)
-        vgg_16_layers = vgg.vgg16(features)
+        # Return VGG16 layers
+        return vgg.vgg16(features)
+
+    def fcn32_vgg_16(self, features, weights, num_classes):
+        # Load VGG16
+        nn_layers = self.load_vgg_16(features, weights)
 
         # Replace dense layers with 1x1 convolution layers
-        pool5 = vgg_16_layers['pool5']
-        conv6_1 = self.conv_layer_1x1(pool5, 4096, 'conv6_1')
-        conv7_1 = self.conv_layer_1x1(conv6_1, 4096, 'conv7_1')
+        pool5 = nn_layers['pool5']
+        pool5_conv1 = self.conv_layer_1x1(pool5, 4096, 'pool5_conv1')
+        nn_layers['pool5_conv1'] = pool5_conv1
+        pool5_conv2 = self.conv_layer_1x1(pool5_conv1, 4096, 'pool5_conv2')
+        nn_layers['pool5_conv2'] = pool5_conv2
 
         # Upsampled to output segmentation using backward stride convolutions
-        fcn32 = self.back_conv_layer(conv7_1, 32, num_classes)
+        fcn32 = self.back_conv_layer(pool5_conv2, 32, num_classes)
 
         # Return logits for FCN-32
-        return fcn32
+        return fcn32, nn_layers
+
+    def fcn16_vgg_16(self, features, weights, num_classes):
+        # Load FCN32
+        _, nn_layers = self.fcn32_vgg_16(features, weights, num_classes)
+
+        # Add 1x1 convolution on top of pool4 layer
+        pool4 = nn_layers['pool4']
+        pool4_conv1 = self.conv_layer_1x1(pool4, num_classes, 'pool4_conv1')
+        nn_layers['pool4_conv1'] = pool4_conv1
+
+        # Fuse previous output with predictions computed on top of:
+        #  - last CNN layer in FCN32 (with a 2x upsampling)
+        pool5_conv2_2x = self.back_conv_layer(nn_layers['pool5_conv2'], 2, num_classes)
+        fuse1 = tf.add(pool4_conv1, pool5_conv2_2x, 'fuse1')
+
+        # Upsampled to output segmentation using backward stride convolutions
+        fcn16 = self.back_conv_layer(fuse1, 16, num_classes)
+
+        return fcn16, nn_layers
+
+    def fcn8_vgg_16(self, features, weights, num_classes):
+        # Load FCN16
+        _, nn_layers = self.fcn16_vgg_16(features, weights, num_classes)
+
+        # Add 1x1 convolution on top of pool3 layer
+        pool3 = nn_layers['pool3']
+        pool3_conv1 = self.conv_layer_1x1(pool3, num_classes, 'pool3_conv1')
+        nn_layers['pool3_conv1'] = pool3_conv1
+
+        # Fuse previous output with predictions computed on top of:
+        #  - last CNN layer in FCN32 (with a 4x upsampling)
+        #  - last CNN layer in FCN16 (with a 2x upsampling)
+        pool4_conv1_2x = self.back_conv_layer(nn_layers['pool4_conv1'], 2, num_classes)
+        fuse1 = tf.add(pool3_conv1, pool4_conv1_2x, 'fuse1')
+        pool5_conv2_4x = self.back_conv_layer(nn_layers['pool5_conv2'], 4, num_classes)
+        fuse2 = tf.add(fuse1, pool5_conv2_4x, 'fuse2')
+
+        # Upsampled to output segmentation using backward stride convolutions
+        fcn8 = self.back_conv_layer(fuse2, 8, num_classes)
+
+        return fcn8, nn_layers
